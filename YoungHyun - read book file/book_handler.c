5c1fe1eb8a1c350a7721fc17cc5792755a32ed94
Poxios <poxios0310@gmail.com>
2021-02-27 10:27:50 +0900

    separated into a function-saveStringByDynamicAlloc

--- YoungHyun - read book file/book_handler.c
+++ YoungHyun - read book file/book_handler.c
@@ -15,13 +15,15 @@ typedef struct __bookStruct
     struct __bookStruct *nextNode;
 } bookStruct;
 
+void saveStringByDynamicAlloc(char **, char *);
+
 int main(void)
 {
-    // Temp string for line input
+    // temp strings for line input
     char tempString[999];
     char *tempTokPtr;
 
-    // dynamic alloc for head node
+    // node to save first node.
     bookStruct *bookHeadNode = (bookStruct *)(malloc(sizeof(bookStruct)));
 
     // dynamic alloc for normal node
@@ -30,40 +32,22 @@ int main(void)
 
     bookHeadNode = bookNode;
 
-    // File handling
+    // file handling
     FILE *bookFileReadStream = fopen("book.txt", "r");
 
     // loop - read until encountering new line
     while ((fscanf(bookFileReadStream, "%[^\n]", tempString)) != EOF)
     {
         // Cut the string and put it into book structure
-        // handle bookNumber
+        // saves integer values using atoi
+        // saves string values using strcpy after malloc function
         bookNode->bookNumber = atoi(strtok(tempString, "|"));
-
-        // handle bookName
-        tempTokPtr = strtok(NULL, "|");
-        bookNode->bookName = (char *)malloc(strlen(tempTokPtr) + 1);
-        strcpy(bookNode->bookName, tempTokPtr);
-
-        // handle publisher
-        tempTokPtr = strtok(NULL, "|");
-        bookNode->publisher = (char *)malloc(strlen(tempTokPtr) + 1);
-        strcpy(bookNode->publisher, tempTokPtr);
-
-        // handle author
-        tempTokPtr = strtok(NULL, "|");
-        bookNode->author = (char *)malloc(strlen(tempTokPtr) + 1);
-        strcpy(bookNode->author, tempTokPtr);
-
+        saveStringByDynamicAlloc(&(bookNode->bookName), strtok(NULL, "|"));
+        saveStringByDynamicAlloc(&(bookNode->publisher), strtok(NULL, "|"));
+        saveStringByDynamicAlloc(&(bookNode->author), strtok(NULL, "|"));
         bookNode->ISBN = atoi(strtok(NULL, "|"));
-
-        // handle location
-        tempTokPtr = strtok(NULL, "|");
-        bookNode->location = (char *)malloc(strlen(tempTokPtr) + 1);
-        strcpy(bookNode->location, tempTokPtr);
-
-        // handle canBorrow
-        bookNode->canBorrow = strtok(NULL, "|")[0];
+        saveStringByDynamicAlloc(&(bookNode->location), strtok(NULL, "|"));
+        bookNode->canBorrow = strtok(NULL, "|")[0]; // no need for dynamic alloc
 
         // dynamic alloc for next node
         // move node index to next node
@@ -86,4 +70,11 @@ int main(void)
         printf("%d | %s | %s | %s | %s | %c | %d\n", bookNode->bookNumber, bookNode->bookName, bookNode->publisher, bookNode->author, bookNode->location, bookNode->canBorrow, bookNode->ISBN);
         bookNode = bookNode->nextNode;
     }
+}
+
+// copy source string to destPtr, with dynamic allocation using malloc
+void saveStringByDynamicAlloc(char **destPtr, char *sourcePtr)
+{
+    *destPtr = (char *)malloc(strlen(sourcePtr) + 1);
+    strcpy(*destPtr, sourcePtr);
 }
